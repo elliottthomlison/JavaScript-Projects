791c70b936312fc195af7d19df051179d19a3849
elliottthomlison <elliott.thomlison@gmail.com>
2021-04-13 01:45:34 +0000

    Adjusting the stylistic settings of the project

--- JavaScript Stopwatch/script.js
+++ JavaScript Stopwatch/script.js
@@ -1,72 +1,63 @@
-window.onload = function () {
+var sw = {
+  // (A) INITIALIZE
+  time : null, // HTML time display
+  reset : null, // HTML reset button
+  start : null, // HTML start/stop button
+  init : function () {
+    // (A1) GET HTML ELEMENTS
+    sw.time = document.getElementById("stopwatch-time");
+    sw.erst = document.getElementById("stopwatch-reset");
+    sw.ego = document.getElementById("stopwatch-start");
+
+    // (A2) ENABLE BUTTON CONTROLS
+    sw.erst.addEventListener("click", stopwatch.reset);
+    sw.erst.disabled = false;
+    sw.ego.addEventListener("click", stopwatch.start);
+    sw.ego.disabled = false;
+  },
+
+  // (B) TIMER ACTION
+  timer : null, // timer object
+  now : 0, // current elapsed time
+  tick : function () {
+    // (B1) CALCULATE HOURS, MINS, SECONDS
+    sw.now++;
+    var remain = sw.now;
+    var hours = Math.floor(remain / 3600);
+    remain -= hours * 3600;
+    var mins = Math.floor(remain / 60);
+    remain -= mins * 60;
+    var secs = remain;
+
+    // (B2) UPDATE THE DISPLAY TIMER
+    if (hours<10) { hours = "0" + hours; }
+    if (mins<10) { mins = "0" + mins; }
+    if (secs<10) { secs = "0" + secs; }
+    sw.time.innerHTML = hours + ":" + mins + ":" + secs;
+  },
   
-    var seconds = 00; 
-    var tens = 00; 
-    var appendMinutes = document.getElementById("minutes")
-    var appendTens = document.getElementById("tens")
-    var appendSeconds = document.getElementById("seconds")
-    var buttonStart = document.getElementById('button-start');
-    var buttonStop = document.getElementById('button-stop');
-    var buttonReset = document.getElementById('button-reset');
-    var Interval ;
-  
-    buttonStart.onclick = function() {
-      
-       clearInterval(Interval);
-       Interval = setInterval(startTimer, 10);
-    }
-    
-      buttonStop.onclick = function() {
-         clearInterval(Interval);
-    }
-    
-  
-    buttonReset.onclick = function() {
-       clearInterval(Interval);
-    minutes = "00";
-        tens = "00";
-            seconds = "00";
-    appendMinutes.innerHTML = minutes;
-      appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
-    }
-     
-     
-    
-    function startTimer () {
-      minutes++
+  // (C) START!
+  start : function () {
+    sw.timer = setInterval(sw.tick, 1000);
+    sw.ego.value = "Stop";
+    sw.ego.removeEventListener("click", stopwatch-start);
+    sw.ego.addEventListener("click", sw.stop);
+  },
 
-      if(minutes <= 9){
-        appendMinutes.innerHTML = "0" + minutes;
-      }
+  // (D) STOP
+  stop  : function () {
+    clearInterval(sw.timer);
+    sw.timer = null;
+    sw.ego.value = "Start";
+    sw.ego.removeEventListener("click", sw.stop);
+    sw.ego.addEventListener("click", stopwatch-start);
+  },
 
-      if(minutes > 9){
-        appendMinutes.innerHTML = minutes;
-      }
-
-      tens++; 
-      
-      if(tens <= 9){
-        appendTens.innerHTML = "0" + tens;
-      }
-      
-      if (tens > 9){
-        appendTens.innerHTML = tens;
-        
-      } 
-      
-      if (tens > 99) {
-        console.log("seconds");
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        appendTens.innerHTML = "0" + 0;
-      }
-      
-      if (seconds > 9){
-        appendSeconds.innerHTML = seconds;
-      }
-    
-    }
-
+  // (E) RESET
+  reset : function () {
+    if (sw.timer != null) { sw.stop(); }
+    sw.now = -1;
+    sw.tick();
   }
+};
+window.addEventListener("load", sw.init);
