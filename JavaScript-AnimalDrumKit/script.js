96e2872fc22636452bbc337a7c0f3867e9ae68ad
elliottthomlison <elliott.thomlison@gmail.com>
2021-05-07 00:55:20 +0000

    Adjust the script formatting, along with further stylistic changes to the font

--- JavaScript-AnimalDrumKit/script.js
+++ JavaScript-AnimalDrumKit/script.js
@@ -2,19 +2,13 @@
   function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) // handle null keys
-      return;
-    audio.currentTime = 0; // allow fast play
     audio.play();
     key.classList.add('playing');
   }
 
-  function endSound (e) {
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!key)
-      return;
-    key.classList.remove('playing');
-  }
-
   window.addEventListener('keydown', playSound)
-  window.addEventListener('keyup', endSound)
+
+//Could use these at a later time but not now
+//if (!audio) => handle null keys
+//return;
+//audio.currentTime = 0; => allow fast play
