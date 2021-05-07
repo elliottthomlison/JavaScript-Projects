bdc98897151a18f0e372fd0d502b73d3e058e653
elliottthomlison <elliott.thomlison@gmail.com>
2021-05-07 00:31:42 +0000

    Rename mthe monkey.mp3 filee

--- JavaScript-AnimalDrumKit/script.js
+++ JavaScript-AnimalDrumKit/script.js
@@ -1,5 +1,20 @@
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key='${e.keycode}'`)
-  if(!audio) return; //stop the function from running all together
-  audio.play();
-});
+  // Solution varies from version prescribed by Wes Bos
+  function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) // handle null keys
+      return;
+    audio.currentTime = 0; // allow fast play
+    audio.play();
+    key.classList.add('playing');
+  }
+
+  function endSound (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!key)
+      return;
+    key.classList.remove('playing');
+  }
+
+  window.addEventListener('keydown', playSound)
+  window.addEventListener('keyup', endSound)
