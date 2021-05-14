5c95233747a135946343c0fc0f8a219a0227f28b
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-14 11:54:53 -0700

    Build the random word/answer functionality

--- JavaScript-Hangman/script.js
+++ JavaScript-Hangman/script.js
@@ -6,3 +6,33 @@
 function unclickable (button) {
   button.disabled = true;
 }
+
+//trying to change the color of the letter based on a click event
+// document.getElementById('aaa').onclick = changeColor;   
+
+//     function changeColor() {
+//         document.body.style.color = "purple";
+//         return false;
+//     }   
+
+//words that will be part of the possible answers
+let answers = [
+  'apple',
+  'bananas',
+  'cookie',
+  'dutch',
+  'elephant',
+  'fingers',
+  'goosebumps'
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+
+function randomWord() {
+  answer = answers[Math.floor(Math.random() * answers.length)];
+  alert(answer)
+}
+randomWord();
