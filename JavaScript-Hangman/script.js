52ddbad628ab3d310826a453d9d00e59f3d54c5b
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-14 15:57:39 -0700

    Add photos of the hangman to the project

--- JavaScript-Hangman/script.js
+++ JavaScript-Hangman/script.js
@@ -12,11 +12,11 @@ let answer = '';
 let maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
+let wordStatus = null;
 
 function randomWord() {
   answer = answers[Math.floor(Math.random() * answers.length)];
 }
-
 
 function generateButtons() {
   let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
@@ -33,6 +33,14 @@ function generateButtons() {
   document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
+//the indexOf portion is to check if it exists because -1 = not existing and anything greater than 0 will 'exist'
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => guessed.indexOf(letter >= 0 ? letter: ' _ ')).join('')
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
 
 randomWord();
-generateButtons()
+generateButtons();
+guessedWord()
