003a3035aa613e7a9a53c25e475a5c870218e012
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-15 12:35:34 -0700

    First version of the TicTacToe

--- JavaScript-TicTacToe/script.js
+++ JavaScript-TicTacToe/script.js
@@ -3,8 +3,23 @@
 // }
 
 function yourTurn() {
-  var x = document.getElementById("test").textContent;
-// document.getElementById("test").innerHTML = x; 
+  var elem = document.getElementsByClassName("XO")
+  // if (elem.value=="X") elem.value = "O";
+  // else elem.value = "X";
+  elem.value = 'X'
+  elem.innerHTML = 'O'
+}
+
+function change( el ) {
+    if ( el.value === "X" )
+        el.value = "O";
+    // else if ( el.value === "O")
+    //     el.value = "X";
+    else 
+        el.value = "X"
 }
 
 // document.querySelector('#test').textContent = "X";
+
+//create a hover + push effect
+
