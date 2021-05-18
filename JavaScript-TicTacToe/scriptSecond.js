0949d5a074a2e49f5190a82a8a2ba59544c49284
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-17 22:26:45 -0700

    Create a setBoardHoverClass function to deal with determining whose turn it is

--- JavaScript-TicTacToe/scriptSecond.js
+++ JavaScript-TicTacToe/scriptSecond.js
@@ -15,8 +15,24 @@ function handleClick(e){
   const currentClass = circleTurn ? o_class : x_class
 
   placeMark(cell, currentClass)
+
+  setBoardHoverClass() 
 }
 
 function placeMark(cell, currentClass){
   cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(x_class)
+  board.classList.remove(o_class)
+  if (circleTurn) {
+    board.classList.add(o_class)
+  } else {
+    board.classList.add(x_class)
+  }
 }
