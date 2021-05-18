24a846a69acccfe02dcd3a1611dba9239abb9c79
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-17 22:31:52 -0700

    Create a startGame function and a winning combination nested array

--- JavaScript-TicTacToe/scriptSecond.js
+++ JavaScript-TicTacToe/scriptSecond.js
@@ -4,7 +4,28 @@ const cellElements = document.querySelectorAll('[data-cell]');
 let circleTurn
 const x_class = 'x'
 const o_class = 'o'
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach(cell => {
+    cell.classList.remove(x_class);
+    cell.classList.remove(o_class);
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+} 
 
 cellElements.forEach(cell => {
   cell.addEventListener('click', handleClick, { once: true })
@@ -36,3 +57,4 @@ function setBoardHoverClass() {
     board.classList.add(x_class)
   }
 }
+
