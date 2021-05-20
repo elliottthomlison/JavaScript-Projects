ad0992824d0ca11cc2fe127237e297789c3ead15
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-19 23:51:50 -0700

    Create variables to build the functionality of the app

--- JavaScript-FourInARow/script.js
+++ JavaScript-FourInARow/script.js
@@ -1,3 +1,4 @@
 //variables
 const board = document.getElementById('board');
 const cellElements = document.querySelectorAll('[data-cell]');
+const winningMessageElement = document.getElementById('winningMessage');
