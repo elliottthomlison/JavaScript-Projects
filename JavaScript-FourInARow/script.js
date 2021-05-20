08dbdcf1f5e69518f2ce0f1740f9ab9d9d2ecbc1
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-19 23:53:09 -0700

    Create variables to build the functionality of the app

--- JavaScript-FourInARow/script.js
+++ JavaScript-FourInARow/script.js
@@ -4,3 +4,7 @@ const cellElements = document.querySelectorAll('[data-cell]');
 const winningMessageElement = document.getElementById('winningMessage');
 const restartButton = document.getElementById('restartButton');
 const winningMessageElement = document.querySelector('[data-winning-message-text');
+
+let blueTurn;
+const blue_class = 'blue'
+const red_class = 'red'
